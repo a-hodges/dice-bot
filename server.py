a5c467f08298c915204cadbe5e0ba7f6c6202d0d
brian-hodges <brjhodges@gmail.com>
2017-09-19 17:04:48 -0500

    Added bot invite website

--- server.py
+++ server.py
@@ -2,14 +2,104 @@
 
 import argparse
 
-from flask import Flask
+from flask import Flask, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy, _QueryProperty
+from sqlalchemy.orm.exc import NoResultFound
+
+import model as m
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Attach Database
+db = SQLAlchemy(app)
+db.Model = m.Base
+# Ugly code to make Base.query work
+m.Base.query_class = db.Query
+m.Base.query = _QueryProperty(db)
+
+
+def create_app(args):
+    r"""
+    Sets up app for use
+    Adds database configuration
+    """
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
+
+    # setup Database
+    app.config['SQLALCHEMY_DATABASE_URI'] = args.database
+    db.create_all()
+
+    # setup config values
+    with app.app_context():
+        # these settings are stored in the configuration table
+        # values here are defaults (and should all be strings or null)
+        # defaults will autopopulate the database when first initialized
+        # when run subsequently, they will be populated from the database
+        # only populated on startup, changes not applied until restart
+        config = {
+            'INVITE_URL': 'https://discordapp.com/oauth2/authorize?client_id=%s&scope=bot&permissions=0',
+            'CLIENT_ID': '',
+        }
+        # get Config values from database
+        for name in config:
+            try:
+                key = m.Config.query.filter_by(name=name).one()
+                config[name] = key.value
+            except NoResultFound:
+                key = m.Config(name=name, value=config[name])
+                db.session.add(key)
+                db.session.commit()
+
+        app.config.update(config)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    html = '''<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dice Bot Invite</title>
+        <link rel="shortcut icon" href="{favicon}">
+        <style>
+        a {{
+            display: inline-block;
+            border: 3px outset grey;
+            background-color: lightgrey;
+            border-radius: 10px;
+        }}
+        a:active {{
+            border-style: inset;
+        }}
+        </style>
+    </head>
+    <body>
+        <h1>Dice Bot</h1>
+        <p>Invite Bot:</p>
+        <a href="{link}">
+            <img src="{favicon}">
+        </a>
+    </body>
+    </html>
+    '''.format(
+        favicon=url_for('favicon'),
+        link=app.config['INVITE_URL'] % app.config['CLIENT_ID'],
+    )
+    return html
+
+
+@app.route('/favicon.ico')
+def favicon():
+    r"""
+    The favorites icon for the site
+    """
+    return send_from_directory(
+        app.root_path,
+        'dice-bot-icon.png',
+        mimetype='image/png',
+    )
 
 if __name__ == '__main__':
     port = 80
@@ -41,6 +131,8 @@ if __name__ == '__main__':
 
     if args.reload:
         app.config['TEMPLATES_AUTO_RELOAD'] = True
+    
+    create_app(args)
 
     app.run(
         host=host,
