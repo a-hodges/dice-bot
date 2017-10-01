19d7d317b20ece88754c28e0783ba6f0470aa96b
brian-hodges <brjhodges@gmail.com>
2017-09-30 19:23:57 -0500

    Separated !iamdone into its own command

--- characters.py
+++ characters.py
@@ -16,46 +16,45 @@ class CharacterCog (Cog):
         A user can only be associated with 1 character at a time
 
         Parameters:
-        [name] is he name of the character to associate
-            to remove character association use !iam done
+        [name] is the name of the character to associate
         '''
-        server = ctx.guild.id
-        if name.lower() == 'done':
-            # remove character association
+        try:
+            character = ctx.session.query(m.Character)\
+                .filter_by(name=name, server=ctx.guild.id).one()
+        except NoResultFound:
+            character = m.Character(name=name, server=ctx.guild.id)
+            ctx.session.add(character)
+            await ctx.send('Creating character: {}'.format(name))
+
+        if character.user is None:
+            character.user = ctx.author.id
             try:
-                character = ctx.session.query(m.Character)\
-                    .filter_by(user=ctx.author.id, server=server).one()
-                character.user = None
-                await ctx.send('{} is no longer playing as {}'.format(
+                ctx.session.commit()
+                await ctx.send('{} is {}'.format(
                     ctx.author.mention, str(character)))
-            except NoResultFound:
+            except IntegrityError:
                 await ctx.send(
-                    '{} does not have a character to remove'.format(
-                        ctx.author.mention))
-
-            ctx.session.commit()
+                    'You are already using a different character')
         else:
-            # associate character
-            try:
-                character = ctx.session.query(m.Character)\
-                    .filter_by(name=name, server=server).one()
-            except NoResultFound:
-                character = m.Character(name=name, server=server)
-                ctx.session.add(character)
-                await ctx.send('Creating character: {}'.format(name))
+            await ctx.send('Someone else is using {}'.format(
+                str(character)))
 
-            if character.user is None:
-                character.user = ctx.author.id
-                try:
-                    ctx.session.commit()
-                    await ctx.send('{} is {}'.format(
-                        ctx.author.mention, str(character)))
-                except IntegrityError:
-                    await ctx.send(
-                        'You are already using a different character')
-            else:
-                await ctx.send('Someone else is using {}'.format(
-                    str(character)))
+    @commands.command()
+    async def iamdone(self, ctx):
+        '''
+        Removes a character association
+        '''
+        try:
+            character = ctx.session.query(m.Character)\
+                .filter_by(user=ctx.author.id, server=ctx.guild.id).one()
+            character.user = None
+            await ctx.send('{} is no longer playing as {}'.format(
+                ctx.author.mention, str(character)))
+        except NoResultFound:
+            await ctx.send(
+                '{} does not have a character to remove'.format(
+                    ctx.author.mention))
+        ctx.session.commit()
 
     @commands.command()
     async def whois(self, ctx, *, member: discord.Member):
