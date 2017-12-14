35de26119fe169a45fdec402c9b6a20f69e9a841
brian-hodges <brjhodges@gmail.com>
2017-12-13 23:03:05 -0600

    Added setup.py
    Prep for distutils

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Dice-bot',
+      version='1.0',
+      description='Discord bot for managing D&D characters',
+      author='Brian Hodges',
+      url='https://github.com/b-hodges/dice-bot',
+      packages=['dicebot'],
+     )
