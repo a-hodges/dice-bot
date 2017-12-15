c83911763b3ba8be02272a9457f2e60642962415
Brian Hodges <brjhodges@gmail.com>
2017-12-14 23:42:02 -0600

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Dice-bot',
-      version='1.0',
+      version='1.0.1',
       description='Discord bot for managing D&D characters',
       author='Brian Hodges',
       url='https://github.com/b-hodges/dice-bot',
