0a2740bfff7e7611d579bce73fe57f2834f282d2
Brian Hodges <brjhodges@gmail.com>
2018-01-09 13:56:27 -0600

    Added extensible unary argument

--- dicebot/util/equations.py
+++ dicebot/util/equations.py
@@ -1,3 +1,7 @@
+'''
+An extensible module for evaluation of arbitrary mathematical expressions
+'''
+
 import operator
 import re
 
@@ -20,6 +24,7 @@ operations = [
 unary = {
     '+': operator.pos,
     '-': operator.neg,
+    '~': operator.neg,
 }
 
 
@@ -34,7 +39,7 @@ def types(stack):
     return next(zip(*stack)) if stack else []
 
 
-def tokenize(expression, operations=operations):
+def tokenize(expression, operations=operations, unary=unary):
     '''
     Parses a mathematic expression into tokens
     '''
@@ -42,8 +47,11 @@ def tokenize(expression, operations=operations):
         def callback(scanner, match):
             return t, match
         return callback
+    operators = {op for ops in operations for op in ops}
+    unary_operators = [op for op in unary if op not in operators]
     tokens = [(r'\s+', 'WHITESPACE')]
     tokens.extend((r'|'.join(map(re.escape, ops)), i) for i, ops in enumerate(operations))
+    tokens.append((r'|'.join(map(re.escape, unary_operators)), 'UNARY'))
     tokens.extend([
         (r'-', len(operations)),  # ???
         (r'\d*\.\d+', 'FLOAT'),
@@ -58,11 +66,11 @@ def tokenize(expression, operations=operations):
     return [(t, m) for t, m in out if t != 'WHITESPACE']
 
 
-def infix2postfix(expression, operations=operations):
+def infix2postfix(expression, operations=operations, unary=unary):
     '''
     Converts an infix expression to a postfix token list
     '''
-    equation = tokenize(expression, operations)
+    equation = tokenize(expression, operations=operations, unary=unary)
     stack = []
     output = []
 
@@ -79,6 +87,8 @@ def infix2postfix(expression, operations=operations):
             while stack[-1][0] != 'PAREN_OPEN':
                 output.append(stack.pop())
             stack.pop()
+        elif type == 'UNARY':
+            stack.append(item)
         elif isinstance(type, int):  # operators
             if prev_type == 'PAREN_OPEN' or isinstance(prev_type, int):
                 # unary operation
@@ -100,7 +110,7 @@ def infix2postfix(expression, operations=operations):
     return output
 
 
-def solve(expression, operations=operations):
+def solve(expression, operations=operations, unary=unary):
     '''
     Solves an infix expression
 
@@ -108,8 +118,13 @@ def solve(expression, operations=operations):
         The keys of each dict should be the operator and the values should be
         a binary function to apply to the operands
         The functions should be able to take int or float arguments
+    
+    Unary is a dict of unary operations
+        The keys of the dict should be the operator and the values should be
+        a unary function to apply to the operands
+        The functions should be able to take int or float arguments
     '''
-    equation = infix2postfix(expression, operations)
+    equation = infix2postfix(expression, operations=operations, unary=unary)
     stack = []
 
     for type, token in equation:
@@ -133,7 +148,7 @@ def solve(expression, operations=operations):
             raise EquationError('Invalid token: {} in {}'.format(token, expression))
 
     if len(stack) != 1:
-        raise EquationError('Missing operator in {}'.format(expression))
+        raise EquationError('Too many operands for operators in {}'.format(expression))
 
     return stack[0]
 
