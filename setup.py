6d3b21e72ce7188d6e6239d1d4112d4e1b59a170
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-01-29 14:17:10 -0600

    Comment out discord.py for now

--- setup.py
+++ setup.py
@@ -4,13 +4,9 @@ from setuptools import setup, find_packages
 
 requires = [
     "equations (>=1.0,<2.0)",
-    "discord.py (>=1.0,<2.0)",
+    # "discord.py (>=1.0,<2.0)",
     "sqlalchemy (>=1.2,<2.0)",
     "psycopg2 (>=2.7,<3.0)",
-]
-
-dependency_links = [
-    "git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py-1.0",
 ]
 
 setup(name='Dice-bot',
@@ -19,6 +15,5 @@ setup(name='Dice-bot',
       author='BHodges',
       url='https://github.com/b-hodges/dice-bot',
       install_requires=requires,
-      dependency_links=dependency_links,
       scripts=['dicebot.py'],
       packages=find_packages())
