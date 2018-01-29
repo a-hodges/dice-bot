d8669c09d233bb31be92bdac38794c6a833ce753
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-01-29 14:01:16 -0600

    Added requires list to setup

--- setup.py
+++ setup.py
@@ -2,10 +2,23 @@
 
 from setuptools import setup, find_packages
 
+requires = [
+    "equations (>=1.0,<2.0)",
+    "discord.py (>=1.0,<2.0)",
+    "sqlalchemy (>=1.2,<2.0)",
+    "psycopg2 (>=2.7,<3.0)",
+]
+
+dependency_links = [
+    "https://github.com/Rapptz/discord.py/tarball/rewrite#egg=discord-py",
+]
+
 setup(name='Dice-bot',
       version='1.0.1',
       description='Discord bot for managing D&D characters',
       author='BHodges',
       url='https://github.com/b-hodges/dice-bot',
+      requires=requires,
+      dependency_links=dependency_links,
       scripts=['dicebot.py'],
       packages=find_packages())
