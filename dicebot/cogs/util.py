f9cf5f5c6d66f16b764244e989fba0371561ea0a
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-02-17 16:13:25 -0600

    Lint

--- dicebot/cogs/util.py
+++ dicebot/cogs/util.py
@@ -59,7 +59,7 @@ async def send_pages(ctx, paginator):
     Displays a set of pages
     '''
     for page in paginator.pages:
-        await util.send_embed(ctx, author=ctx.author, page)
+        await send_embed(ctx, author=ctx.author, page)
 
 
 def invalid_subcommand(ctx):
