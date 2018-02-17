74a2a3f3a4b0f95ad9eb354f12d6534719b8dbf3
b-hodges <16946799+b-hodges@users.noreply.github.com>
2018-02-17 16:27:24 -0600

    Added embed to prefix change

--- dicebot/__init__.py
+++ dicebot/__init__.py
@@ -178,7 +178,7 @@ async def setprefix(ctx, prefix: str = default_prefix):
         ctx.session.rollback()
         raise Exception('Could not change prefix, an unknown error occured')
     else:
-        await ctx.send('Prefix changed to `{}`'.format(prefix))
+        await util.send_embed(ctx, author=False, description='Prefix changed to `{}`'.format(prefix))
 
 
 prefix = __name__ + '.cogs.'
